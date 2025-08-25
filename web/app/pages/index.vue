f9becf1c3b9185da4067eb82d734e3b27d6e1da2
Matheus Mortari <matheus.felipe.19rt@gmail.com>
2025-08-25 16:32:04 -0300

    chore(css): update styles for the web app

--- web/app/pages/index.vue
+++ web/app/pages/index.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="flex flex-col items-center p-4 md:p-12">
     <header class="flex flex-col items-center gap-4 text-center">
-      <icon name="simple-icons:nuxt" size="50" class="text-primary" />
       <h1>
-        Nuxt.js Starter
+        Nuxt.js Starter 💚🏔️
       </h1>
 
-      <p class="max-w-2xl text-lg text-primary">
+      <p class="max-w-2xl text-lg leading-5 text-primary">
         A full-stack starter setup for Nuxt 4. Built with modern tools and best practices to help you get up and running quickly.
       </p>
     </header>
@@ -14,7 +13,7 @@
     <hr class="my-8 w-full md:w-4/5">
 
     <section class="flex flex-col items-center text-center">
-      <p class="mb-4 max-w-2xl border-b border-primary pb-4 text-center text-sm">
+      <p class="mb-4 max-w-3xl border-b border-primary pb-4 text-center text-sm">
         The Base Package includes everything you need to start a Nuxt project: TypeScript, ESLint, Tailwind CSS, Pinia, OAuth authentication, Prisma, and analytics integration. It's the perfect foundation to build upon.
       </p>
 
@@ -50,18 +49,17 @@
         Quickly scaffold your project using our CLI tool and start building.
       </p>
 
-      <div class="relative w-full max-w-2xl">
-        <button class="absolute right-2 top-2 text-[#d4d4d4] hover:text-primary" @click="copyToClipboard">
-          <icon name="ph:copy" size="20" />
-        </button>
-        <pre class="font-mono overflow-auto rounded-md bg-secondary p-4 text-left text-sm text-[#d4d4d4]">
-npx @matimortari/nuxtjs-starter
-cd &lt;project-name&gt;
-npm install
-npm run db:migrate  <span class="text-[#6b7280]"># or npm run db:push</span>
-npm run dev
-</pre>
-      </div>
+      <article class="card flex flex-col p-0">
+        <div class="flex items-center justify-between gap-4 p-2 text-start text-sm leading-5 text-muted-foreground">
+          <p>
+            > After installing, run the following commands to get started:
+          </p>
+          <button class="hover:scale-sm flex items-center transition-all" title="Copy to Clipboard" @click="copyToClipboard">
+            <icon :name="copyIcon" size="20" />
+          </button>
+        </div>
+        <Shiki lang="bash" :code="commands.join('\n')" class="code-block" />
+      </article>
     </section>
 
     <hr class="my-8 w-full md:w-4/5">
@@ -69,15 +67,23 @@ npm run dev
 </template>
 
 <script setup lang="ts">
-const commands = `npx @matimortari/nuxtjs-starter
-cd <project-name>
-npm install
-npm run db:migrate
-npm run dev`
+const copyIcon = ref("ph:copy")
+
+const commands = [
+  "npx @matimortari/nuxtjs-starter",
+  "cd <project-name>",
+  "npm install",
+  "npm run db:migrate",
+  "npm run dev",
+]
 
 async function copyToClipboard() {
   try {
-    await navigator.clipboard.writeText(commands)
+    await navigator.clipboard.writeText(commands.join("\n"))
+    copyIcon.value = "ph:check"
+    setTimeout(() => {
+      copyIcon.value = "ph:copy"
+    }, 1000)
   }
   catch (error: any) {
     console.error("Failed to copy text: ", error)
